5825d16a84b83b2fc52b824496eb6e571aec220e
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-03-10 19:41:51 +0800

    menambahkan library

---
+++ file1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+
+    srand (time(0));
+    x= rand() % 10;
+
+    if (x==5){
+        x= x+1;
+        cout<< "nilai x sudah dikoreksi"
+    }
+}
