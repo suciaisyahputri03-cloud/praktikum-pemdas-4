6b5dd903188b01faf58b35aeae4fc7445dbb1abd
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-03-10 19:50:58 +0800

    menambahkan int main

--- file1.cpp
+++ file1.cpp
@@ -9,6 +9,8 @@ int main(){
 
     if (x==5){
         x= x+1;
-        cout<< "nilai x sudah dikoreksi"
+        cout<< "nilai x sudah dikoreksi menjadi :"<<x<<endl;
     }
+
+    cout << "bilangan yang diberikan = "<<x;
 }
