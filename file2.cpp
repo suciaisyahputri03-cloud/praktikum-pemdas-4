3b389093690dd8af547cd78c6d2aa5467c9d323c
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-03-10 20:20:41 +0800

    menambahkan input nilai bahasa inggris dan matematika

--- file2.cpp
+++ file2.cpp
@@ -19,4 +19,9 @@ string status_lulus(float r){
 int main(){
     float nilBI,nilMT,rerata;
     string status;
+
+    cout <<"masukkan nilai bahasa inggris =";
+    cin >> nilBI;
+    cout <<"masukkan nilai matematika =";
+    cin >> nilMT;
 }
